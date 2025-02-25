76eca2f6adad947a0fd3a454cdb5690e077e3116
ro77en <lucas.mdantas@icloud.com>
2025-02-25 14:35:36 -0300

    feat: read questions from .txt file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import dao.QuestionDAO;
+import model.Question;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try {
+            QuestionDAO x = new QuestionDAO();
+            List<Question> q = x.getQuestions();
+
+            for (Question y : q) {
+                System.out.println(y);
+            }
+
+        } catch (IOException e) {
+            System.out.printf("Error accessing file: %s", e.getMessage());
+        }
+
     }
 }
---
+++ src/dao/QuestionDAO.java
@@ -0,0 +1,39 @@
+package dao;
+
+import model.Question;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionDAO {
+    public List<Question> getQuestions() throws IOException{
+        File file = new File("src/resources/forms.txt");
+        List<Question> questions = new ArrayList<>();
+
+        try (FileReader fr = new FileReader(file); BufferedReader br = new BufferedReader(fr)) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(" - ", 2);
+
+                if (parts.length < 2) {
+                    System.out.printf("Invalid question format: %s", line);
+                    continue;
+                }
+
+                int questionId = Integer.parseInt(parts[0].trim());
+                String questionText = parts[1].trim();
+
+                Question question = new Question(questionId, questionText);
+
+                questions.add(question);
+            }
+        }
+
+        return questions;
+    }
+}
