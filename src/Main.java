60e929da69ab2b824576113d01cd209b57c3820d
ro77en <lucas.mdantas@icloud.com>
2025-02-27 10:07:02 -0300

    feat: add menu and user view flow

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,36 @@
+import controller.MenuController;
 import controller.QuestionController;
 import controller.UserController;
 import dao.QuestionDAO;
 import dao.UserDAO;
+import model.Question;
 import service.QuestionService;
 import service.UserService;
+import view.MenuView;
 import view.UserView;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserDAO userDAO = new UserDAO();
         QuestionDAO questionDAO = new QuestionDAO();
-
-        UserService userService = new UserService(userDAO);
         QuestionService questionService = new QuestionService(questionDAO);
-
-        UserController userController = new UserController(userService, questionService);
         QuestionController questionController = new QuestionController(questionService);
 
-        UserView userView = new UserView(userController, questionController);
+        UserDAO userDAO = new UserDAO();
+        UserService userService = new UserService(userDAO);
+        UserController userController = new UserController(userService);
+
 
         try {
-            userView.showFormsQuestions();
+            List<Question> questions = questionController.getQuestionsFromService();
+            UserView userView = new UserView(userController, questions);
+
+            MenuController menuController = new MenuController(userView);
+            MenuView menuView = new MenuView(menuController);
+
+            menuView.showMenu();
         } catch (IOException | RuntimeException e) {
             System.out.println(e.getMessage());
         }
